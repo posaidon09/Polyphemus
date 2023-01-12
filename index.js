8c2410c623719692b6646876caafe669de39a4a6
* <halohalo090909@gmail.com>
2023-01-12 15:34:44 +0100

    translation update

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ const config = {
 // CODE
 const Client = require("./src/Client");
 const client = new Client(config);
+const Salvis = require("salvis")
+
+client.on("messageCreate", function(message){
+    if (!message.channel.id === "1061018646010417182") return
+    else if (message.channel.id === "1061018646010417182") {
+        if (!message.content.includes("praise blåhaj")) {
+            
+            message.delete()
+        } else if (message.content.includes("praise blåhaj")) return
+        }
+    
+})
+
